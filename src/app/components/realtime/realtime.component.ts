e6a94a02f08d3f167e00369ece2a316fb76b303b
kozawjyee <kozawgyi551997@gmail.com>
2022-07-11 04:36:41 +0630

    [timer]

--- src/app/components/realtime/realtime.component.ts
+++ src/app/components/realtime/realtime.component.ts
@@ -3,6 +3,8 @@ import { DataService } from 'src/app/services/data.service';
 import { FormatService } from 'src/app/services/format.service';
 import { ApiService } from 'src/app/shares/api.service';
 import * as moment from 'moment';
+import { Subscription, timer } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 const data = {
     btc: "343543",
@@ -21,6 +23,8 @@ export class RealtimeComponent implements OnInit {
 
   luckyNumber: string;
   currentDate: Date = new Date();
+  timerSubscription: Subscription;
+  reachTime: boolean = false;
 
   @Input() dateTime: string;
 
@@ -58,27 +62,48 @@ export class RealtimeComponent implements OnInit {
   
   async ngOnInit() {
     this.luckyNumber = this.getRandomInt(0,99);
-    const getTime = this.currentDate.toLocaleString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true }).toString();
-  
+    const today = moment().format('Y-MM-DD');
+    let getTime = this.currentDate.toLocaleString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true }).toString();
+    const todayNumber = await this.dataService.getTodayNumber(today);
+
     let startNumber: any = setInterval(() => {
       this.luckyNumber = this.getRandomInt(0, 99);
     }, 6000);
 
-    console.log(getTime === '8:20 PM');
+    this.timerSubscription = timer(0, 6000).pipe(
+      map(() => {
+        const currentTime = moment().format('h:mm A');
+        if(todayNumber){
+          const getLuckyNumber = todayNumber.filter((value) => value.lucky_number !== null);
 
-    if(getTime === '8:20 PM') {
-      clearInterval(startNumber);
-      setTimeout(() => {
-        startNumber = setInterval(() => {
-          this.luckyNumber = this.getRandomInt(0,99);
-        },6000);
-      }, 10000);
-    }
+          getLuckyNumber.map((value, index) => {
+            
+            const luckyHour = moment(new Date(value.schedule_time).getTime()).format('h:mm A');
+            const interval = moment(new Date(value.schedule_time).getTime() + 30*60000).format('h:mm A');
+
+            if( ( currentTime >= luckyHour && currentTime <= interval ) && this.reachTime === false){
+              clearInterval(startNumber);
+              this.reachTime = true;
+              this.luckyNumber = value.lucky_number.toString();
+              setTimeout(() => {
+                startNumber = setInterval(() => {
+                  this.luckyNumber = this.getRandomInt(0,99);
+                },6000);
+                this.reachTime = false;
+              }, 1800000);
+              return;
+            }
+            else{
+              return;
+            }
+          });
+        }
+      })
+    ).subscribe();
+
 
     const getBTC = await this.api.getRequest('prices/BTC-MMK/sell');
     const getETC = await this.api.getRequest('prices/ETC-MMK/sell');
-    // const today = moment().format('Y-MM-DD');
-    // const getLucky = await this.dataService.getTodayNumber(today);
 
     this.rates = {
       btc : getBTC.data ? getBTC.data : null,
